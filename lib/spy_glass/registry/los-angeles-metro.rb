60e56ede32923ebb3012cef139dc668f21e9faa5
Tony Mendoza <mendozatony@outlook.com>
2018-06-24 15:22:21 -0700

    Update los-angeles-metro.rb

--- lib/spy_glass/registry/los-angeles-metro.rb
+++ lib/spy_glass/registry/los-angeles-metro.rb
@@ -24,19 +24,8 @@ SpyGlass::Registry << SpyGlass::Client::Socrata.new(opts) do |collection|
         '$where' => <<-WHERE.oneline
           id = IS NOT NULL
         WHERE
-  })
+        })
     }
-      
-    # Should return ONE item, the route and run
-    SpyGlass::Registry << SpyGlass::Client::Socrata.new(routeOpts) do |runCollection|
-      features = $runCollection.values[0].map do |item|
-        title = <<-TITLE.oneline
-          #{item['id']}
-        TITLE
-      'properties' => item.merge('title' => title)
-      end
-      {'type' => 'FeatureCollection', 'features' => features}
-    end
     
     title = <<-TITLE.oneline
     #{SpyGlass::Salutations.next} Vehicle no. #{item['id']} on route #{item['route_id']} and run #{item['run_id']}.
