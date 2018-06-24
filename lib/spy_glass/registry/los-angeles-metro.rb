7513dd7737d36b2c74e4e79a37f3af8087737683
Tony Mendoza <mendozatony@outlook.com>
2018-06-24 13:16:29 -0700

    Testing run loop

--- lib/spy_glass/registry/los-angeles-metro.rb
+++ lib/spy_glass/registry/los-angeles-metro.rb
@@ -17,6 +17,19 @@ opts = {
 
 SpyGlass::Registry << SpyGlass::Client::Socrata.new(opts) do |collection|
     features = collection.values[0].map do |item|
+     
+     routeOpts = {
+      path: '/los-angeles-metro',
+      cache: SpyGlass::Cache::Memory.new(expires_in: 1200),
+      source: 'http://api.metro.net/agencies/lametro/vehicles/' + item['route_id'] + '/runs/' + item['run_id'] + '/'
+    }
+      
+    # Should return ONE item, the route and run
+    runItem = Array.new
+    SpyGlass::Registry << SpyGlass::Client::Base.new(routeOpts) do |runCollection|
+      runItem = runCollection.values[0].map do |item|
+      end
+    end
       
     title = <<-TITLE.oneline
     #{SpyGlass::Salutations.next} Vehicle no. #{item['id']} on route: ;.
