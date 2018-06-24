108a552d42b86700991c9188a5f511c574c73fdb
Tony Mendoza <mendozatony@outlook.com>
2018-06-24 12:52:22 -0700

    Remove "path" for routeOpts

--- lib/spy_glass/registry/los-angeles-metro.rb
+++ lib/spy_glass/registry/los-angeles-metro.rb
@@ -19,7 +19,6 @@ SpyGlass::Registry << SpyGlass::Client::Socrata.new(opts) do |collection|
     features = collection.values[0].map do |item|
       
      routeOpts = {
-      path: '/los-angeles-metro/routes/',
       cache: SpyGlass::Cache::Memory.new(expires_in: 1200),
       source: 'http://api.metro.net/agencies/lametro/vehicles/' + item['route_id'] + '/runs/' + item['run_id'] + '/'
     }
