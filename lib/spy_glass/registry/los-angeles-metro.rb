0c5bb1ae62b38a9b293342a59827620aa3cc2ae0
Tony Mendoza <mendozatony@outlook.com>
2018-06-24 12:23:37 -0700

    Testing usefulness improvements for Metro Bus

--- lib/spy_glass/registry/los-angeles-metro.rb
+++ lib/spy_glass/registry/los-angeles-metro.rb
@@ -3,16 +3,35 @@ require 'spy_glass/registry'
 opts = {
   path: '/los-angeles-metro',
   cache: SpyGlass::Cache::Memory.new(expires_in: 1200),
-  source: 'http://api.metro.net/agencies/lametro/vehicles/?'
+  source: 'http://api.metro.net/agencies/lametro/vehicles/?'+Rack::Utils.build_query({
+    '$where' => <<-WHERE.oneline
+      id = IS NOT NULL AND
+      heading IS NOT NULL AND
+      id = IS NOT NULL AND
+      longitude IS NOT NULL AND
+      latitude IS NOT NULL AND
+      predictable = true 
+    WHERE
+  })
 }
 
 SpyGlass::Registry << SpyGlass::Client::Socrata.new(opts) do |collection|
     features = collection.values[0].map do |item|
+      
+     routeOpts = {
+      path: '/los-angeles-metro/routes/',
+      cache: SpyGlass::Cache::Memory.new(expires_in: 1200),
+      source: 'http://api.metro.net/agencies/lametro/vehicles/' + item['route_id'] + '/runs/' + item['run_id'] + '/'
+    }
+    # Should return ONE item, the route and run
+    SpyGlass::Registry << SpyGlass::Client::Socrata.new(routeOpts) do |collection|
+      runItem = collection.values[0].map[0]
+      
     title = <<-TITLE.oneline
-    #{SpyGlass::Salutations.next} Vehicle no. #{item['id']} on route: #{item['route_id']};.
+    #{SpyGlass::Salutations.next} Vehicle no. #{item['id']} on route: #{runItem['display_name']};.
     Last reported #{item['seconds_since_report']} seconds ago.
     TITLE
-
+      
     {
       'id' => item['id'],
       'type' => 'Feature',
