25f243870f0b88f182e5ac2a5f83aa917efd1f98
Tony Mendoza <mendozatony@outlook.com>
2018-06-24 15:44:42 -0700

    Update los-angeles-metro.rb

--- lib/spy_glass/registry/los-angeles-metro.rb
+++ lib/spy_glass/registry/los-angeles-metro.rb
@@ -14,38 +14,17 @@ opts = {
   })
 }
 
-$runCollection = nil
-
 SpyGlass::Registry << SpyGlass::Client::Socrata.new(opts) do |collection|
     features = collection.values[0].map do |item|
-     
-     routeOpts = {
-      path: '/los-angeles-metro',
-      cache: SpyGlass::Cache::Memory.new(expires_in: 1200),
-      source: 'http://api.metro.net/agencies/lametro/routes/' + item['route_id'] + '/runs/' + item['run_id'] + '/?'+Rack::Utils.build_query({
-        '$where' => <<-WHERE.oneline
-          id = IS NOT NULL
-        WHERE
-        })
-    }
-      
-      
-    # Should return ONE item, the route and run
-    SpyGlass::Registry << SpyGlass::Client::Socrata.new(routeOpts) do |$runCollection|
-      runFeatures = $runCollection.values[0].map do |runItem|
-        runTitle = <<-TITLE.oneline
-          #{runItem['id']}
-        TITLE
-        {
-          'properties' => runItem.merge('title' => runTitle)
-        }
-      end
-      {'type' => 'FeatureCollection', 'features' => runFeatures}
-    end
+    
+    url = URI('http://api.metro.net/agencies/lametro/routes/' + item['route_id'] + '/runs/' + item['run_id'] + '/')
+    connection = Faraday.new(url: url.to_s)
+    response = connection.get
+    routeItem = JSON.parse(response.body).values[0].map[0]
     
     title = <<-TITLE.oneline
     #{SpyGlass::Salutations.next} Vehicle no. #{item['id']} on route #{item['route_id']} and run #{item['run_id']}.
-    Last reported #{item['seconds_since_report']} seconds ago. #{$runCollection.length} 
+    Last reported #{item['seconds_since_report']} seconds ago. #{routeItem['id']} 
     TITLE
       
     {
