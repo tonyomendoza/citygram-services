46f56abb1cfb2b349d88d4cac2b009c1e2d632e8
Tony Mendoza <mendozatony@outlook.com>
2018-06-26 17:13:46 -0700

    Update los-angeles-metro-rail.rb

--- lib/spy_glass/registry/los-angeles-metro-rail.rb
+++ lib/spy_glass/registry/los-angeles-metro-rail.rb
@@ -8,11 +8,29 @@ opts = {
 
 SpyGlass::Registry << SpyGlass::Client::Socrata.new(opts) do |collection|
     features = collection.values[0].map do |item|
-    title = <<-TITLE.oneline
-    #{SpyGlass::Salutations.next} Vehicle no. #{item['id']} on route: #{item['route_id']};.
-    Last reported #{item['seconds_since_report']} seconds ago.
-    TITLE
 
+    title = "#{SpyGlass::Salutations.next} Vehicle no. #{item['id']} on route #{item['route_id']}"
+
+    # Returns the run
+    routeUrl = URI("http://api.metro.net/agencies/lametro-rail/routes/#{item['route_id']}/runs/#{item['run_id']}/")
+    routeConnection = Faraday.new(url: routeUrl.to_s)
+    routeResponse = routeConnection.get
+    route = JSON.parse(routeResponse.body)
+
+    hasRoute = true
+    if !route["message"].nil?
+        if route["message"].include? "no run with that id"
+            hasRoute = false
+            route.store("display_name", "")
+            route.store("direction_name", "")
+            title = title + ", an undetermined run (#{item['run_id']})."
+        end
+    else
+        route.store("message", "")
+        title = title + ", run #{route['display_name']}."
+    end
+
+    # Set up JSON here
     {
       'id' => item['id'],
       'type' => 'Feature',
@@ -23,7 +41,11 @@ SpyGlass::Registry << SpyGlass::Client::Socrata.new(opts) do |collection|
           item['latitude'].to_f
         ]
       },
-      'properties' => item.merge('title' => title)
+      'properties' => item.merge('title' => title),
+      'route' => {
+        'direction_name' => route['direction_name'],
+        'display_name' => route['display_name']
+        }
     }
   end
 
